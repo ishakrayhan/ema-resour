808809d2ba5c34d2c805ac06d3bf604bb576c288
unknown <ishakrayhan>
2023-10-23 22:45:43 +0600

    added thr product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,18 @@ import'./Product.css'
 
 const Product = (props) => {
     
-    const {img,name,sellar,quantity,}=props.product
+    const {img,price,name,seller,quantity,ratings}=props.product;
     return (
-        <div>
-           <h1>this is Product</h1>
+        <div className='product'>
+          
            <img src={img} alt="" /> 
+          <div className='product-info'>
+          <h6 className='product-name'>{name}</h6>
+           <p>Price: ${price}</p>
+           <p>Manufacturer: {seller}</p>
+           <p>Rating: {ratings} Stars</p>
+          </div>
+          <button className='btn-card'>Add to Card</button>
         </div>
     );
 };
